ac3d45f313459c2d8139e594a8e545bedfd1c15a
Ryan Manchester <ryanm17@gmail.com>
2018-07-26 15:44:52 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -39,7 +39,7 @@ class FiguresController < ApplicationController
 
   patch '/figures/:id' do
     @figure = Figure.find(params[:id])
-    binding.pry
+  #  binding.pry
     if !params[:landmark]["name"].empty?
       @figure.landmarks.each do |landmark|
         landmark.update(name: params[:landmark]["name"])
