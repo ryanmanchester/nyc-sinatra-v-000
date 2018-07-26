14ef5704bffadf76466711dcf7fbc5e321f1b098
Ryan Manchester <ryanm17@gmail.com>
2018-07-26 01:18:15 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -6,6 +6,38 @@ class FiguresController < ApplicationController
   end
 
   get '/figures/new' do
-    "Hello World"
+    erb :'/figures/new'
+  end
+
+  get '/figures/:id' do
+    #binding.pry
+    @figure = Figure.find(params[:id])
+     erb :'/figures/show'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(name: params["figure"]["name"])
+
+    if !params[:title]["name"].empty?
+    @figure.titles << Title.create(params[:title])
+   else
+     @figure.title_ids = params[:figure]["title_ids"]
+   end
+
+    if !params[:landmark]["name"].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    else
+      @figure.landmark_ids = params[:figure]["landmark_ids"]
+    end
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+
+    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do
+    binding.pry
   end
 end
--- config.ru
+++ config.ru
@@ -10,4 +10,5 @@ end
 use Rack::MethodOverride
 
 use LandmarksController
+use FiguresController
 run ApplicationController
